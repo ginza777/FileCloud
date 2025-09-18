8c4426607350d11df5f48ae999f029e2091b6ec4
sherzamon <ginzapro.uz@gmail.com>
2025-09-19 03:11:34 +0500

    good

--- core/urls.py
+++ core/urls.py
@@ -41,6 +41,8 @@ urlpatterns = [
     path('api/', include('apps.core_api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('api/bot/', include('apps.bot.urls')),
 ]
 
 if settings.DEBUG:
